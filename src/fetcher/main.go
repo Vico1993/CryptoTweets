ccbf8cc237536c928b34c59cf226938d48e146eb
Victor Piolin <victor.piolin@culiniste.ca>
2020-08-04 21:01:08 -0400

    Move Mongo application in is own container and .env file

--- src/fetcher/main.go
+++ src/fetcher/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -15,18 +16,27 @@ func main() {
 		log.Fatal(err)
 	}
 
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017").SetAuth(options.Credential{AuthSource: "cryptotweets", Username: "crypto", Password: "secret"}))
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI("mongodb://database:27017").SetAuth(
+			options.Credential{
+				AuthSource: os.Getenv("MONGO_INITDB_DATABASE"),
+				Username:   os.Getenv("MONGO_USER"),
+				Password:   os.Getenv("MONGO_USER_PWD"),
+			}))
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer client.Disconnect(ctx)
 
-	db := client.Database("cryptotweets")
+	db := client.Database(os.Getenv("MONGO_INITDB_DATABASE"))
 
 	for _, crypto := range cryptos {
 		_, err := db.Collection("cryptocurrency").InsertOne(ctx, crypto)
